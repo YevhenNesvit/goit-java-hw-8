1ab6e227e847be186300e9afc014fa32f9b799b6
Yevhen Nesvit <86827674+YevhenNesvit@users.noreply.github.com>
2022-06-01 10:59:51 +0300

    Update Print.java

--- Print.java
+++ Print.java
@@ -1,11 +1,5 @@
 public class Print {
-    public static void print() {
-        Shape[] shapes = new Shape[5];
-        shapes[0] = new Circle("Circle",11);
-        shapes[1] = new Rectangle("Rectangle", 2, 11);
-        shapes[2] = new Quad("Quad", 11);
-        shapes[3] = new Trapezium("Trapezium", 2, 11, 20);
-        shapes[4] = new Triangle("Triangle", 2, 11);
+    public static void print(Shape[] shapes) {
 
         for (Shape shape : shapes) {
             System.out.println("Name is " + shape.getName() + ", area = " + shape.getArea());
