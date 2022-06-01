f575a937ebc8642ea1f8bc67a8a154a90ca4deef
Yevhen Nesvit <86827674+YevhenNesvit@users.noreply.github.com>
2022-06-01 10:58:35 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Print.print();
+        Shape[] shapes = new Shape[5];
+        shapes[0] = new Circle("Circle",11);
+        shapes[1] = new Rectangle("Rectangle", 2, 11);
+        shapes[2] = new Quad("Quad", 11);
+        shapes[3] = new Trapezium("Trapezium", 2, 11, 20);
+        shapes[4] = new Triangle("Triangle", 2, 11);
+        Print.print(shapes);
     }
 }
